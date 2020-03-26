334800067beb2b5e82c6f2d56d574b941c1af1b7
Mikhail S <python2020@rambler.ru>
2020-03-26 20:57:10 +0300

    progKievUaMarathon_2020-03-26_02

---
+++ mar_001145_L02_task04_readNumMax.java
@@ -0,0 +1,33 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+import java.util.Scanner;
+
+public class mar_001145_L02_task04_readNumMax {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("input num1: ");
+        var num1 = sc.nextInt();
+        System.out.println("input num2: ");
+        var num2 = sc.nextInt();
+        System.out.println("input num3: ");
+        var num3 = sc.nextInt();
+        System.out.println("input num4: ");
+        var num4 = sc.nextInt();
+        int temp;
+        if (num1 > num2) {
+
+        } else {
+            num1 = num2;
+        }
+        if (num1 > num3) {
+
+        } else {
+            num1 = num3;
+        }
+        if (num1 > num4) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num4);
+        }
+    }
+}
