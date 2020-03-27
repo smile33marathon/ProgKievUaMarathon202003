6a8c37d54923dc1ace92d6d7d1ee2466fb41dee6
Mikhail S <python2020@rambler.ru>
2020-03-27 06:37:28 +0300

    progKievUaMarathon_2020-03-27_04_Mar_001145_L03P01_02_Loops_doWhile

---
+++ Mar_001145_L03P01_02_Loops_doWhile.java
@@ -0,0 +1,11 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+public class Mar_001145_L03P01_02_Loops_doWhile {
+    public static void main(String[] args) {
+        int n = 10;
+        do {
+            System.out.println(n);
+            n -= 1;
+        } while (n > 0);
+    }
+}
