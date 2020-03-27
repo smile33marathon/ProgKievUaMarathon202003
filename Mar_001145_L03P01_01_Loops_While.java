1b68cc982b9e408ea28d7452e6fb110875dfd91f
Mikhail S <python2020@rambler.ru>
2020-03-27 06:26:59 +0300

    progKievUaMarathon_2020-03-27_03_Mar_001145_L03P01_01_Loops_While

---
+++ Mar_001145_L03P01_01_Loops_While.java
@@ -0,0 +1,13 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+public class Mar_001145_L03P01_01_Loops_While {
+    public static void main(String[] args) {
+        int n = 10;
+
+        while (n > 0) {
+            System.out.println(n);
+            n = n - 1;
+        }
+        System.out.println("end");
+    }
+}
