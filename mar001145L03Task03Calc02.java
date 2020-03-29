8510aa813419130090c18028eafbbd195522ced9
Mikhail S <python2020@rambler.ru>
2020-03-29 11:07:40 +0300

    progKievUaMarathon001145_2020-03-29_03_L03_T03_Calc_upd03

---
+++ mar001145L03Task03Calc02.java
@@ -0,0 +1,31 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+import java.util.Scanner;
+
+public class mar001145L03Task03Calc02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("");
+            System.out.println("Input math operation [+], [-], [*], [/] or [return] to exit: ");
+//            System.out.println("op");
+            String op = sc.next();
+            System.out.print("Input \"a\": ");
+            int a = sc.nextInt();
+            System.out.print("Input \"b\": ");
+            int b = sc.nextInt();
+
+            if (op.equals("+")) {
+                System.out.println("[a] + [b]: " + (a + b));
+            } else if (op.equals("-")) {
+                System.out.println("[a] - [b]: " + (a - b));
+            } else if (op.equals("*")) {
+                System.out.println("[a] * [b]: " + (a * b));
+            } else if (op.equals("/")) {
+                System.out.println("[a] / [b]: " + (a / b));
+            } else if (op.equals("")){
+                break;
+            }
+        }
+    }
+}
