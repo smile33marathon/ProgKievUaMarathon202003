d0e457ae55c05ca13ebd3372ae3637398554c9ca
Mikhail S <python2020@rambler.ru>
2020-03-29 10:42:02 +0300

    progKievUaMarathon001145_2020-03-29_03_L03

---
+++ mar001145L03Task01Loops.java
@@ -0,0 +1,21 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+/*Вывести на консоль числа от 1 до 50 всеми возможными циклами.*/
+
+public class mar001145L03Task01Loops {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println("[for]: " + i);
+        }
+        int j = 1;
+        while (j <= 50) {
+            System.out.println("[while]: " + j);
+            j++;
+        }
+        int k = 1;
+        do {
+            System.out.println("[do while]: " + k);
+            k++;
+        } while (k <= 50);
+    }
+}
---
+++ mar001145L03Task02Div3.java
@@ -0,0 +1,14 @@
+package ua.kiev.prog02.ProgKievUaMarathon202003;
+
+/*Вывести на консоль числа от 1 до 100, которые
+делятся на 3 без остатка. Например, 3, 6, 9, 12...*/
+
+public class mar001145L03Task02Div3 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
